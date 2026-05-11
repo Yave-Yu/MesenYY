312fd4300031109f711bbefd1e3bae624fb5b0c8
Yave Yu <Yave-Yu@users.noreply.github.com>
2026-05-11 08:45:35 +0800

    Revert version back to 2.1.1, use own version number instead
    Simplified about window, no need to tell casual gamer something unimportant.
    Also updated icon, that white is not FFFFFF but FCFCFC, so I changed it to FFFFFF.

--- Core/Shared/EmuSettings.cpp
+++ Core/Shared/EmuSettings.cpp
@@ -135,8 +135,8 @@ void EmuSettings::Serialize(Serializer& s)
 uint32_t EmuSettings::GetVersion()
 {
 	uint16_t major = 2;
-	uint8_t minor = 2;
-	uint8_t revision = 0;
+	uint8_t minor = 1;
+	uint8_t revision = 1;
 	return (major << 16) | (minor << 8) | revision;
 }
 
