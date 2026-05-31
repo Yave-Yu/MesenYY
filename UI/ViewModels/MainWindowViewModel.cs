c095425d159959b6171119ba37e1780f1ae6120d
Yave Yu <Yave-Yu@users.noreply.github.com>
2026-05-31 08:55:45 +0800

    Revert main window name back to "Mesen" for better compatibility with external program
    Also changed GUID to avoid single-instance interactions with official Mesen2 and other forks (Mesen2 SA-1, MesenCE, Nexen, etc.).

--- UI/ViewModels/MainWindowViewModel.cs
+++ UI/ViewModels/MainWindowViewModel.cs
@@ -27,7 +27,7 @@ namespace Mesen.ViewModels
 		[Reactive] public AudioPlayerViewModel? AudioPlayer { get; private set; }
 		[Reactive] public RecentGamesViewModel RecentGames { get; private set; }
 
-		[Reactive] public string WindowTitle { get; private set; } = "MesenYY";
+		[Reactive] public string WindowTitle { get; private set; } = "Mesen";
 		[Reactive] public Size RendererSize { get; set; }
 
 		[Reactive] public bool IsMenuVisible { get; set; }
@@ -85,7 +85,7 @@ namespace Mesen.ViewModels
 
 		private void UpdateWindowTitle()
 		{
-			string title = "MesenYY";
+			string title = "Mesen";
 			string romName = RomInfo.GetRomName();
 			if(!string.IsNullOrWhiteSpace(romName)) {
 				title += " - " + romName;
