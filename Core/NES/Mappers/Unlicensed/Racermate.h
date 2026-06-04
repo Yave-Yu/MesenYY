b46a24b0d19200f318d5edae655641a103d634b3
Yave Yu <Yave-Yu@users.noreply.github.com>
2026-06-04 14:05:20 +0800

    NES: Improved CHR-NVRAM support

--- Core/NES/Mappers/Unlicensed/Racermate.h
+++ Core/NES/Mappers/Unlicensed/Racermate.h
@@ -12,7 +12,7 @@ protected:
 	uint16_t GetChrPageSize() override { return 0x1000; }
 	uint32_t GetChrRamSize() override { return 0x10000; }
 	uint32_t GetSaveRamSize() override { return 0; }
-	bool ForceChrBattery() override { return true; }
+	bool ForceChrBattery() override { return !_romInfo.IsNes20Header; }
 	bool EnableCpuClockHook() override { return true; }
 
 	void InitMapper() override
@@ -53,6 +53,31 @@ protected:
 				_console->GetCpu()->ClearIrqSource(IRQSource::External);
 				break;
 		}
+	}
 
+	void LoadBattery() override
+	{
+		if(HasBattery() && _saveRamSize > 0) {
+			_emu->GetBatteryManager()->LoadBattery("NES", ".sav", _saveRam, _saveRamSize);
+		}
+
+		if(_hasChrBattery && _saveChrRamSize > 0) {
+			_emu->GetBatteryManager()->LoadBattery("NES", ".chr.sav", _chrRam + (_chrRamSize - _saveChrRamSize), _saveChrRamSize);
+		} else if(_hasChrBattery) {
+			_emu->GetBatteryManager()->LoadBattery("NES", ".chr.sav", _chrRam + (_chrRamSize / 2), _chrRamSize / 2);
+		}
+	}
+
+	void SaveBattery() override
+	{
+		if(HasBattery() && _saveRamSize > 0) {
+			_emu->GetBatteryManager()->LoadBattery("NES", ".sav", _saveRam, _saveRamSize);
+		}
+
+		if(_hasChrBattery && _saveChrRamSize > 0) {
+			_emu->GetBatteryManager()->SaveBattery("NES", ".chr.sav", _chrRam + (_chrRamSize - _saveChrRamSize), _saveChrRamSize);
+		} else if(_hasChrBattery) {
+			_emu->GetBatteryManager()->SaveBattery("NES", ".chr.sav", _chrRam + (_chrRamSize / 2), _chrRamSize / 2);
+		}
 	}
 };
