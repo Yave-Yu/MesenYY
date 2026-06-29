046aa650587721cae11a8f66a8580a56c70d2f67
Yave Yu <Yave-Yu@users.noreply.github.com>
2026-06-29 20:16:34 +0800

    I just wonder which game used BnuyRom mapper
    I included this so earlier before I release V2 (before rename to MesenYY), from
    smaldragon/Mesen2.

--- Core/NES/Mappers/Homebrew/BnuyRom.h
+++ Core/NES/Mappers/Homebrew/BnuyRom.h
@@ -21,6 +21,7 @@ private:
 	uint8_t   _irqPrescaler = 0;
 	uint8_t   _irqCounter = 0;
 	int       _banksPerWindow = 0;
+
 protected:
 	uint16_t GetPrgPageSize() override { return 0x8000; }
 	uint16_t GetChrPageSize() override { return 0x0800; }
@@ -36,7 +37,7 @@ protected:
 	}
 	
 	void UpdateIrqSource() {
-		if (_irqCounter == 0 && _romInfo.SubMapperID & 4) {
+		if(_irqCounter == 0 && _romInfo.SubMapperID & 4) {
 			_console->GetCpu()->SetIrqSource(IRQSource::External);
 		} else {
 			_console->GetCpu()->ClearIrqSource(IRQSource::External);
@@ -48,7 +49,7 @@ protected:
 		_flash.reset(new FlashSST39SF040(_prgRom, _prgSize));
 		_orgPrgRom = vector<uint8_t>(_prgRom, _prgRom + _prgSize);
 		
-		for (int i = 0; i < 4; i++) {
+		for(int i = 0; i < 4; i++) {
 			_chrBanks[i] = GetPowerOnByte();
 		}
 
@@ -58,44 +59,44 @@ protected:
 		UpdatePrgRegister(0);
 		UpdateIrqSource();
 		
-		// Linear Mode
-		if ( (_romInfo.SubMapperID & 3) == 0) {
-			if (GetMirroringType() == MirroringType::FourScreens) {
+		//Linear Mode
+		if( (_romInfo.SubMapperID & 3) == 0) {
+			if(GetMirroringType() == MirroringType::FourScreens) {
 				SetPpuMemoryMapping(0x0000, 0x3FFF, ChrMemoryType::ChrRam, 0x0000, MemoryAccessType::ReadWrite);
 			} else {
 				SetPpuMemoryMapping(0x0000, 0x1FFF, ChrMemoryType::ChrRam, 0x0000, MemoryAccessType::ReadWrite);
 			}
 		}
-		// Shared Mode
-		else if ( (_romInfo.SubMapperID & 3) == 1)
+		//Shared Mode
+		else if( (_romInfo.SubMapperID & 3) == 1)
 		{
-			for (int i = 0; i < 4; i++) {
+			for(int i = 0; i < 4; i++) {
 				SelectChrPage(i, _chrBanks[i] );
 			}
 			
-			if (GetMirroringType() == MirroringType::FourScreens) {
-				// Nametables
+			if(GetMirroringType() == MirroringType::FourScreens) {
+				//Nametables
 				SetPpuMemoryMapping(0x2000, 0x27FF, ChrMemoryType::ChrRam, 0x0000, MemoryAccessType::ReadWrite);
 				SetPpuMemoryMapping(0x2800, 0x2FFF, ChrMemoryType::ChrRam, 0x7800, MemoryAccessType::ReadWrite);
-				// Bonus Ram
+				//Bonus RAM
 				SetPpuMemoryMapping(0x3000, 0x37FF, ChrMemoryType::ChrRam, 0x0000, MemoryAccessType::ReadWrite);
 				SetPpuMemoryMapping(0x3800, 0x3FFF, ChrMemoryType::ChrRam, 0x7800, MemoryAccessType::ReadWrite);
 			}
 		}
-		// Independent Mode
-		else if ( (_romInfo.SubMapperID & 3) == 2) 
+		//Independent Mode
+		else if( (_romInfo.SubMapperID & 3) == 2) 
 		{
 			_banksPerWindow = _chrRamSize/4/0x0800;
 
-			for (int i = 0; i < 4; i++) {
+			for(int i = 0; i < 4; i++) {
 				SelectChrPage(i, (_chrBanks[i]%_banksPerWindow) + (_banksPerWindow*i) );
 			}
 			
-			if (GetMirroringType() == MirroringType::FourScreens) {
-				// Nametables
+			if(GetMirroringType() == MirroringType::FourScreens) {
+				//Nametables
 				SetPpuMemoryMapping(0x2000, 0x27FF, ChrMemoryType::ChrRam, 0x7800, MemoryAccessType::ReadWrite);
 				SetPpuMemoryMapping(0x2800, 0x2FFF, ChrMemoryType::ChrRam, 0xF800, MemoryAccessType::ReadWrite);
-				// Bonus RAM
+				//Bonus RAM
 				SetPpuMemoryMapping(0x3000, 0x37FF, ChrMemoryType::ChrRam, 0x17800, MemoryAccessType::ReadWrite);
 				SetPpuMemoryMapping(0x3800, 0x3FFF, ChrMemoryType::ChrRam, 0x1F800, MemoryAccessType::ReadWrite);
 			}
@@ -148,17 +149,17 @@ protected:
 				UpdatePrgRegister(value);
 				break;
 			case 0xC000:
-				if (_romInfo.SubMapperID & 4) {
+				if(_romInfo.SubMapperID & 4) {
 					_irqCounter = value;
 					UpdateIrqSource();
 				}
 				break;
 			case 0xE000:
-				if ( (_romInfo.SubMapperID & 3) == 1) {
+				if( (_romInfo.SubMapperID & 3) == 1) {
 					_chrBanks[addr&3] = value;
 					SelectChrPage(addr&3, value);
 				}
-				else if ( (_romInfo.SubMapperID & 3) == 2) {
+				else if( (_romInfo.SubMapperID & 3) == 2) {
 					_chrBanks[addr&3] = value;
 					SelectChrPage(addr&3, (_chrBanks[addr&3]%_banksPerWindow) + (_banksPerWindow*(addr&3)) );
 				}
@@ -173,11 +174,11 @@ protected:
 		vector<MapperStateEntry> entries;
 		entries.push_back(MapperStateEntry("$8000.0-5", "PRG Bank", _prgBank & 0x3F, MapperStateValueType::Number8));
 		entries.push_back(MapperStateEntry("$8000.6-7", "WRAM Bank", _prgBank >> 6, MapperStateValueType::Number8));
-		if ( _romInfo.SubMapperID & 4 ) {
+		if( _romInfo.SubMapperID & 4 ) {
 			entries.push_back(MapperStateEntry("$C000", "IRQ Timer", _irqCounter, MapperStateValueType::Number8));
 			entries.push_back(MapperStateEntry("$C000", "IRQ Prescaler", _irqPrescaler, MapperStateValueType::Number8));
 		}
-		if ((_romInfo.SubMapperID & 3) != 0) {
+		if((_romInfo.SubMapperID & 3) != 0) {
 			entries.push_back(MapperStateEntry("$E000", "CHR Bank 0", _chrBanks[0], MapperStateValueType::Number8));
 			entries.push_back(MapperStateEntry("$E001", "CHR Bank 1", _chrBanks[1], MapperStateValueType::Number8));
 			entries.push_back(MapperStateEntry("$E002", "CHR Bank 2", _chrBanks[2], MapperStateValueType::Number8));
@@ -188,11 +189,11 @@ protected:
 	
 	uint8_t MapperReadVram(uint16_t addr, MemoryOperationType memoryOperationType) override
 	{
-		if (addr < 0x2000) {
+		if(addr < 0x2000) {
 			_irqPrescaler = 0;
 		} else {
 			_irqPrescaler++;
-			if ((_irqPrescaler & 0x7) == 0x4) {
+			if((_irqPrescaler & 0x7) == 0x4) {
 				_irqCounter--;
 				UpdateIrqSource();
 			}
