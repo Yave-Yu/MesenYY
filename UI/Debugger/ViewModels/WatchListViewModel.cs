4e9e53c0dc162be6bbfa46610b71548cf2a4a456
Yave Yu <Yave-Yu@users.noreply.github.com>
2026-09-21 08:56:21 +0800

    Debugger: Watch - Keep selection when moving entry

--- UI/Debugger/ViewModels/WatchListViewModel.cs
+++ UI/Debugger/ViewModels/WatchListViewModel.cs
@@ -183,6 +183,7 @@ namespace Mesen.Debugger.ViewModels
 					IsEnabled = () => Selection.SelectedItems.Count == 1 && Selection.SelectedIndex > 0,
 					OnClick = () => {
 						MoveUp(Selection.SelectedIndex);
+						Selection.SelectedIndex--;
 					}
 				},
 
@@ -193,6 +194,7 @@ namespace Mesen.Debugger.ViewModels
 					IsEnabled = () => Selection.SelectedItems.Count == 1 && Selection.SelectedIndex < WatchEntries.Count - 2,
 					OnClick = () => {
 						MoveDown(Selection.SelectedIndex);
+						Selection.SelectedIndex--;
 					}
 				},
 
